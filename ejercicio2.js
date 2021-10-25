08c490f8f2ce79a83ec092aeb9ea02381d9c7138
Daniel Hernández <est.daniele.hernan1@unimilitar.edu.co>
2021-10-25 10:58:39 -0500

    Signed-off-by: Daniel Hernández <est.daniele.hernan1@unimilitar.edu.co>

--- ejercicio2.js
+++ ejercicio2.js
@@ -40,7 +40,10 @@ function init() {
    
     Cubo = []; //define un array unidimensional
     dim = 10; //valor inicial de las dimensiones de los cubos
-    angulo = 20*(2*Math.PI)/(360); //valor del angulo de giro
+    angulo = Math.PI/4; //valor del angulo de giro
+    d2 = dim/2
+    di = Math.sqrt(Math.pow(d2, 2)+Math.pow(d2, 2));
+    desplazamiento = di - d2;
 
     Cubo.push(cubo(dim, dim, dim, 0xFF0000, 'Physical', false));
     Cubo.push(cubo(dim, dim, dim, 0xFFDD00, 'Physical', false));
@@ -48,9 +51,9 @@ function init() {
 
     //un ciclo for para optimizar la creación de los cubos y hacer sus respectivas transformaciones
     for(i=0; i < 3; i++){
-        Cubo[i].translateX(dim/2); //translada el cubo en el eje x 4 unidades
-        Cubo[i].translateY(dim/2); //translada el cubo en el eje y 4 unidades
-        Cubo[i].translateZ(dim/2); //translada el cubo en el eje z 4 unidades
+        Cubo[i].translateX(desplazamiento); //translada el cubo en el eje x las unidades nesesarias para que el vértice del primer cubo toque el eje x según el ángulo rotado
+        Cubo[i].translateY(dim/2); //translada el cubo en el eje y 5 unidades
+        Cubo[i].translateZ(desplazamiento); //translada el cubo en el eje z las unidades necesarias para que el vértice del primer cubo toque el eje z según el ángulo rotado
     }
 
     for(i=0; i < 3; i++){
@@ -59,19 +62,17 @@ function init() {
             unidades=(dim/2)+(dim/4)+((((dim/2)+(dim/4))/2))*(i-1);//da la posición respectiva a cada cubo
             Cubo[i].scale.set(escala, escala, escala); //cambia la posición de los cubos respectivamente
             Cubo[i].translateY(unidades); //translada el cubo en el eje y para que quede arriba del cubo anterior
-
-            Cubo[0].rotateY(angulo);//rota el cubo 1 según el valor del ángulo
-            Cubo[2].rotateY(angulo);//rota el cubo 3 según el valor del ángulo
         }
     }
 
-    /*Cubo[0].rotateY(angulo);
-    Cubo[2].rotateY(angulo*3);*/
+    Cubo[0].rotateY(angulo);//rota el cubo 1 según el valor del ángulo
+    Cubo[2].rotateY(angulo);//rota el cubo 3 según el valor del ángulo
+
   
     light = new THREE.PointLight(0xFFFFFF);
     light2 = new THREE.PointLight(0xFFFFFF);
                                         
-    light.position.set(-10, 50, 10);
+    light.position.set(-10, 20, 10);
     light2.position.set(10, 50, -10);
     scene.add( light );
     scene.add( light2 ); 
